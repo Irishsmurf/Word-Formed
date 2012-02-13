5a953bce752650f4a8fa1c2975852c946a5c76eb
Dave <davidsmurf@gmail.com>
2012-02-13 18:11:04 +0000

    Added HiScore Activity Class and hiscore layout

---
+++ Word-Formed/src/com/google/corrigan/owen/wordformed/HiScore.java
@@ -0,0 +1,17 @@
+package com.google.corrigan.owen.wordformed;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+
+public class HiScore extends Activity
+{
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.hiscore);
+		
+	}
+}
