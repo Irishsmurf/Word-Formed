8f1e1135a5a622b1cb0ae1bc4acd29a1f5c55646
Owen Corrigan <owen.corrigan@gmail.com>
2012-02-13 22:17:55 +0100

    Main user interface buttons have been connected to listeners so they can do stuff. Connected HiScore class to button. Added HiScore Activity to manifest
    Changed quit button into help button because quit buttons aren't needed
    in android and 4 buttons is a neater in landscape mode
    
    4 legs good, 2 legs bad

--- Word-Formed/src/com/google/corrigan/owen/wordformed/HiScore.java
+++ Word-Formed/src/com/google/corrigan/owen/wordformed/HiScore.java
@@ -3,15 +3,12 @@ package com.google.corrigan.owen.wordformed;
 import android.app.Activity;
 import android.os.Bundle;
 
-
-
 public class HiScore extends Activity
 {
 	@Override
 	public void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.hiscore);
-		
+		setContentView(R.layout.hiscore);	
 	}
 }
--- Word-Formed/src/com/google/corrigan/owen/wordformed/WordFormed.java
+++ Word-Formed/src/com/google/corrigan/owen/wordformed/WordFormed.java
@@ -1,13 +1,44 @@
 package com.google.corrigan.owen.wordformed;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 
-public class WordFormed extends Activity {
+public class WordFormed extends Activity implements OnClickListener{
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        //Set up click listeners for all the buttons
+        View newGameButton = findViewById(R.id.new_game_button);
+        newGameButton.setOnClickListener(this);
+		View multiplayerButton = findViewById(R.id.multiplayer_button);
+		multiplayerButton.setOnClickListener(this);
+		View how_to_play_button = findViewById(R.id.how_to_play_button);
+		how_to_play_button.setOnClickListener(this);
+		View highScoresButton = findViewById(R.id.high_scores_button);
+		highScoresButton.setOnClickListener(this);
     }
+
+	public void onClick(View v)
+	{
+		switch(v.getId())
+		{
+			case R.id.new_game_button:
+				break;
+			case R.id.multiplayer_button:
+				break;
+			case R.id.how_to_play_button:
+				break;
+			case R.id.high_scores_button:
+				Intent i = new Intent(this, HiScore.class);
+				startActivity(i);
+				break;
+		}
+		
+	}
 }
