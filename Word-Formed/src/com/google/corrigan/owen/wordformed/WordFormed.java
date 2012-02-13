327b6b64178c8039122e049c20228b17ec9653cd
Owen Corrigan <owen.corrigan@gmail.com>
2012-02-13 16:35:53 +0100

    Start of user UI. Landscape mode is broken - in demonware I would totally get paedobear right about now...

--- Word-Formed/src/com/google/corrigan/owen/wordformed/WordFormed.java
+++ Word-Formed/src/com/google/corrigan/owen/wordformed/WordFormed.java
@@ -1,13 +1,9 @@
 package com.google.corrigan.owen.wordformed;
 
-//David Kernan is really gay
-//Lols
-//Test change
-//Owen is actually quite the jolly fellow
 import android.app.Activity;
 import android.os.Bundle;
 
-public class WordFormedActivity extends Activity {
+public class WordFormed extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
