f0d14e3191ca6bb45bd819feb638a9f164c9b6cd
Owen Corrigan <owen.corrigan@gmail.com>
2012-02-28 13:53:24 +0000

    Dat Object Oriented code!

--- Word-Formed/src/com/google/corrigan/owen/wordformed/DraggableBox.java
+++ Word-Formed/src/com/google/corrigan/owen/wordformed/DraggableBox.java
@@ -1,6 +1,75 @@
 package com.google.corrigan.owen.wordformed;
 
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.util.Log;
+import android.view.MotionEvent;
+
 public class DraggableBox
 {
-
+	private float rectX = 50;
+	private float rectY = 60;
+	private int rectSize = 80;
+	private RectF rect = new RectF(rectX, rectY, rectX+rectSize, rectY+rectSize);
+	private boolean dragging = false;
+	
+	public void draw(Canvas canvas)
+	{
+		//Draw Rectangle
+		Paint dragRectangle = new Paint();
+		dragRectangle.setColor(Color.BLUE);
+		canvas.drawRect(rect, dragRectangle);
+	}
+	
+	public boolean onTouchEvent(MotionEvent event)
+	{
+		float mouseX = event.getX();
+		float mouseY = event.getY();
+		Log.d("WORDFORMED", "MouseX = "+ mouseX + ", Mouse Y = " + mouseY);
+		switch(event.getAction())
+		{
+			case MotionEvent.ACTION_DOWN:
+				if(mouseX + 10 > rectX && 
+						mouseX - 10 < rectX + rectSize && 
+						mouseY + 10 > rectY && 
+						mouseY - 10 < rectY + rectSize)
+				{
+					//offsetX = mouseX - rectX;
+					//offsetY = mouseY - rectY;
+					dragging = true;
+					Log.d("WORDFORMED", "Inside Rectangle");
+				}
+				else dragging = false;
+				break;
+			case MotionEvent.ACTION_UP:
+				/*
+				Log.d("MOUSEUP", "Mouse X = " + mouseX + " Mouse Y = " + mouseY );
+				if(mouseX > snapX && 
+						mouseX < snapX + rectSize && 
+						mouseY > snapY && 
+						mouseY <  snapY + rectSize)
+				{
+					Log.d("MOUSEUP", "Inside the Snapper!" );
+					rectX = snapX;
+					rectY = snapY;
+					Log.d("WORDFORMED", "Mouse up and inside snapX");
+					rect = new RectF(rectX, rectY, rectX+rectSize, rectY+rectSize);
+					rectX -= 40;
+					rectY -= 40;
+				}
+				dragging = false;
+				*/
+				break;
+			default:
+				if(dragging)
+				{
+					rectX = mouseX;
+					rectY = mouseY;
+					rect = new RectF(rectX - 40, rectY - 40, rectX+rectSize - 40, rectY+rectSize - 40);
+				}
+		}
+		return true;
+	}
 }
--- Word-Formed/src/com/google/corrigan/owen/wordformed/SinglePlayerGameView.java
+++ Word-Formed/src/com/google/corrigan/owen/wordformed/SinglePlayerGameView.java
@@ -11,16 +11,7 @@ import android.view.View;
 
 public class SinglePlayerGameView extends View
 {
-	private float rectX = 50;
-	private float rectY = 60;
-	private int rectSize = 80;
-	private float offsetX;
-	private float offsetY;
-	private RectF rect = new RectF(rectX, rectY, rectX+rectSize, rectY+rectSize);
-	private boolean dragging = false;
-	private int snapX = 300;
-	private int snapY = 300;
-	private RectF snapBox = new RectF(snapX, snapY, snapX+rectSize, snapY+rectSize);
+	DraggableBox db = new DraggableBox();
 	
 	public SinglePlayerGameView(Context context)
 	{
@@ -35,70 +26,15 @@ public class SinglePlayerGameView extends View
 		background.setColor(getResources().getColor(
 				R.color.background));
 		canvas.drawRect(0, 0, getWidth(), getHeight(), background);
-		
-		//Draw Rectangle
-		Paint snap = new Paint();
-		snap.setColor(Color.RED);
-		canvas.drawRect(snapBox, snap);
 				
-		//Draw Rectangle
-		Paint dragRectangle = new Paint();
-		dragRectangle.setColor(Color.BLUE);
-		canvas.drawRect(rect, dragRectangle);
+		db.draw(canvas);
 	}
 	
 	@Override
 	public boolean onTouchEvent(MotionEvent event)
 	{
-		float mouseX = event.getX();
-		float mouseY = event.getY();
-		Log.d("WORDFORMED", "MouseX = "+ mouseX + ", Mouse Y = " + mouseY);
-		switch(event.getAction())
-		{
-			case MotionEvent.ACTION_DOWN:
-				if(mouseX + 10 > rectX - offsetX && 
-						mouseX - 10 < rectX - offsetX + rectSize && 
-						mouseY + 10 > rectY - offsetY && 
-						mouseY - 10 < rectY + rectSize - offsetY)
-				{
-					//offsetX = mouseX - rectX;
-					//offsetY = mouseY - rectY;
-					dragging = true;
-					Log.d("WORDFORMED", "Inside Rectangle");
-				}
-				else dragging = false;
-				invalidate();
-				break;
-			case MotionEvent.ACTION_UP:
-				Log.d("MOUSEUP", "Mouse X = " + mouseX + " Mouse Y = " + mouseY );
-				if(mouseX > snapX && 
-						mouseX < snapX + rectSize && 
-						mouseY > snapY && 
-						mouseY <  snapY + rectSize)
-				{
-					Log.d("MOUSEUP", "Inside the Snapper!" );
-					rectX = snapX;
-					rectY = snapY;
-					Log.d("WORDFORMED", "Mouse up and inside snapX");
-					rect = new RectF(rectX, rectY, rectX+rectSize, rectY+rectSize);
-					rectX -= 40;
-					rectY -= 40;
-				}
-				//offsetX = 0;
-				//offsetY = 0;
-				dragging = false;
-				invalidate();
-				break;
-			default:
-				if(dragging)
-				{
-					rectX = mouseX;
-					rectY = mouseY;
-					rect = new RectF(rectX-offsetX-40, rectY-offsetY-40, rectX+rectSize-offsetX-40, rectY+rectSize-offsetY-40);
-					invalidate();
-					
-				}
-		}
+		db.onTouchEvent(event);
+		invalidate();
 		return true;
 	}
 }
