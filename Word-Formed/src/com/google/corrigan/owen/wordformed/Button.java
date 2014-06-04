14a38b20b274f13ceede806cc803635497e85a21
Dave <davidsmurf@gmail.com>
2014-06-04 21:24:33 +0100

    That shouldn't be there

--- Word-Formed/src/com/google/corrigan/owen/wordformed/Button.java
+++ Word-Formed/src/com/google/corrigan/owen/wordformed/Button.java
@@ -5,7 +5,6 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.RectF;
-import android.graphics.Typeface;
 import android.util.Log;
 import android.view.MotionEvent;
 
