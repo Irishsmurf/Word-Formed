14183288ec6dc1edfd8fcbb5b4237a9c6232ac96
Irishsmurf <dave@paddez.com>
2018-07-07 14:05:53 +0100

    Format Dictionary.java

--- Word-Formed/src/com/google/corrigan/owen/wordformed/Dictionary.java
+++ Word-Formed/src/com/google/corrigan/owen/wordformed/Dictionary.java
@@ -16,76 +16,62 @@ import android.util.Log;
 
 public class Dictionary
 {
-	private static ArrayList<String> data;
-	
-	public Dictionary(Context context)
-	{
-		doLoad(context);
-	}
-	
-	public static boolean isWord(String word)
-	{
-		int index = Collections.binarySearch(data, word);
-		return index >= 0;
-	}
-	
-	@SuppressWarnings("unchecked")
-	private void doLoad(Context context)
-	{
-		InputStream fileIn;
-		ObjectInputStream in;
-		try
-		{	
-			fileIn = context.getResources().openRawResource(R.raw.words);
-			in = new ObjectInputStream(fileIn);
-		
-			data = (ArrayList<String>) in.readObject();
-			System.out.println(data.size());
-			in.close();
-			fileIn.close();
-			
-		}
-		catch(Exception e)
-		{
-			Log.d("Dictionary", e.getCause()+"");
-		}
-	}
-	
-	public static int size()
-	{
-		return data.size();
-	}
-	
-	public void doSave(Context context)
-	{
-		try
-		{
-			InputStream raw = context.getResources().openRawResource(R.raw.word_list);
-			Log.d("Dictionary", ""+raw.available());
-			Scanner in = new Scanner(raw);
-			FileOutputStream out = new FileOutputStream("words.dict");
-			ObjectOutput obj = new ObjectOutputStream(out);
-			while(in.hasNextLine())
-			{
-				String line = in.nextLine();
-				data.add(line);
-				Log.d("Dictionary", line+" added to ArrayList. Total : "+data.size());
-			}
-			
-			try
-			{
-				obj.writeObject(data);
-			}
-			finally
-			{
-				obj.close();
-			}
-			
-		}
-		catch(IOException e)
-		{
-			Log.d("Dictionary", "Nope, not saving Either"+e.getCause());
-		}
-		
-	}
+  private static ArrayList<String> data;
+
+  public Dictionary(Context context) {
+    doLoad(context);
+  }
+
+  public static boolean isWord(String word) {
+    int index = Collections.binarySearch(data, word);
+    return index >= 0;
+  }
+
+  @SuppressWarnings("unchecked")
+  private void doLoad(Context context) {
+    InputStream fileIn;
+    ObjectInputStream in;
+    try {
+      fileIn = context.getResources().openRawResource(R.raw.words);
+      in = new ObjectInputStream(fileIn);
+
+      data = (ArrayList<String>) in.readObject();
+      System.out.println(data.size());
+      in.close();
+      fileIn.close();
+
+    }
+    catch(Exception e) {
+      Log.d("Dictionary", e.getCause()+"");
+    }
+  }
+
+  public static int size() {
+    return data.size();
+  }
+
+  public void doSave(Context context) {
+    try {
+      InputStream raw = context.getResources().openRawResource(R.raw.word_list);
+      Scanner in = new Scanner(raw);
+      FileOutputStream out = new FileOutputStream("words.dict");
+      ObjectOutput obj = new ObjectOutputStream(out);
+      Log.d("Dictionary", "" + raw.available());
+
+      while(in.hasNextLine()) {
+        String line = in.nextLine();
+        data.add(line);
+        Log.d("Dictionary", line + " added to ArrayList Total: " + data.size());
+      }
+
+      try {
+        obj.writeObject(data);
+      }
+      finally {
+        obj.close();
+      }
+    } catch(IOException e) {
+      Log.d("Dictionary", "Nope, not saving Either: " + e.getCause());
+    }
+  }
 }
