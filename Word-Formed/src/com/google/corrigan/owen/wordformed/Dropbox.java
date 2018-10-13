d382724fd07054593b89987cb1d177e117d229b1
David K <dave@paddez.com>
2018-10-13 20:03:45 +0100

    Formatting

--- Word-Formed/src/com/google/corrigan/owen/wordformed/Dropbox.java
+++ Word-Formed/src/com/google/corrigan/owen/wordformed/Dropbox.java
@@ -9,92 +9,76 @@ import android.graphics.RectF;
 import android.graphics.Typeface;
 import android.util.Log;
 
-public class Dropbox
-{
-	//X, Y, width and height
+public class Dropbox {
 	private int border = 5;
-	//Outer and inner rectangles used to draw it
 	private RectF dragBorder, dragFill;
-	//Max number of boxes stored
-	//Array of references to Draggable boxes stored at certain position
 	private LinkedList<DraggableBox> tiles = new LinkedList<DraggableBox>();
 	@SuppressWarnings("unused")
 	private final String TAG = "DRAGGABLEBOX";
+
 	Paint paint = new Paint();
 	String instructions;
-	//Constructor. Takes x, y, height and width and parameters
-	public Dropbox(int x, int y, int width, int height)
-	{
+
+	public Dropbox(int x, int y, int width, int height) {
 		dragBorder = new RectF(x, y, x + width, y + height);
 		dragFill = new RectF(x + border, y + border, x + width - border, y + height - border);
-		if(y == 325)
-			instructions = "Save letters here";
-		else
+		if(y == 325) {
+		    instructions = "Save letters here";
+		} else {
 			instructions = "Form words here";
+		}
 		paint.setColor(R.color.black);
 		paint.setTextSize(30);
 		paint.setTypeface(Typeface.MONOSPACE);
 		paint.setAlpha(150);
 	}
-	
-	public String tilesToString()
-	{
+
+	public String tilesToString() {
 		String word = "";
-		for(DraggableBox tile: tiles)
-		{
+		for(DraggableBox tile: tiles) {
 			word += tile.getLetter();
 		}
 		Log.d("DropBox", word);
 		return word;
 	}
-	
-	public int getScore()
-	{
+
+	public int getScore() {
 		int val = 0;
-		for(DraggableBox tile: tiles)
-		{
+		for(DraggableBox tile: tiles) {
 			val += tile.getValue();
 		}
 		return val;
 	}
-	
+
 	//Method for Adding tiles to the Dropbox
-	public void add(DraggableBox d)
-	{
+	public void add(DraggableBox d) {
 		//size of 7 tiles held.
-		if(tiles.size() == 7)
-		{
+		if(tiles.size() == 7) {
 			DraggableBox tmp = getFirst();
 			tmp.move(-100, dragBorder.bottom - 75);
 			tiles.remove(tmp);
 		}
 		tiles.add(d);
-		//Call the Button to check if it should light up
 		Button.checkWord();
 		updatePositions();
 	}
-	
-	public void updatePositions()
-	{
-		for(int i = 0; i < tiles.size(); i++)
+
+	public void updatePositions() {
+		for(int i = 0; i < tiles.size(); i++) {
 			tiles.get(i).move(i*65+20, dragBorder.bottom - 75);
+        }
 	}
-	
-	//Remove a tile from the Box.
-	public void remove(DraggableBox d)
-	{
+
+	public void remove(DraggableBox d) {
 		tiles.remove(d);
 		Button.checkWord();
 		updatePositions();
 	}
-	
+
 	@SuppressWarnings("unchecked")
-	public synchronized void removeAll()
-	{
+	public synchronized void removeAll() {
 		LinkedList<DraggableBox> clone = (LinkedList<DraggableBox>) tiles.clone();
-		for(DraggableBox tile: clone)
-		{
-			
+		for(DraggableBox tile: clone) {
 			DraggableBox tmp = getFirst();
 			tmp.move(-100, dragBorder.bottom - 60);
 			tiles.remove(tmp);		
@@ -102,15 +86,12 @@ public class Dropbox
 		Button.opaque();
 		updatePositions();
 	}
-	
-	public DraggableBox getFirst()
-	{
+
+	public DraggableBox getFirst() {
 		return tiles.get(0);
 	}
-	
-	//Draws method. Takes canvas as parameter
-	public void draw(Canvas canvas)
-	{
+
+	public void draw(Canvas canvas) {
 		Paint dragRectangle = new Paint();
 		dragRectangle.setColor(Color.BLACK);
 		canvas.drawRect(dragBorder, dragRectangle);
@@ -118,10 +99,8 @@ public class Dropbox
 		canvas.drawRect(dragFill, dragRectangle);
 		canvas.drawText(instructions, dragBorder.left + 80, dragBorder.top + 60, paint);
 	}
-	
-	//Contains method. Returns true is point is within bounds of drop box
-	public boolean contains(float x, float y)
-	{
+
+	public boolean contains(float x, float y) {
 		return dragBorder.contains(x, y);
 	}
 }
