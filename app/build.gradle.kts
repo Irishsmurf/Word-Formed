997621989b9faed884017505642069c2baa26eb3
David Kernan <dave@paddez.com>
2026-03-04 10:48:25 +0000

    Bump version to 1.1.1 for first automated release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.paddez.wordformed"
         minSdk = 24
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.1.0"
+        versionCode = 5
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
