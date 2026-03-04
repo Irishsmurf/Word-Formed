7f877935111417cdbc0813c0e038df74619e368c
David Kernan <dave@paddez.com>
2026-03-04 12:43:43 +0000

    Bump version to 1.2.0 for major UX and animation update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.paddez.wordformed"
         minSdk = 24
         targetSdk = 35
-        versionCode = 6
-        versionName = "1.1.2"
+        versionCode = 7
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
