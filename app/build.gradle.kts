00a57c41ee9eda0ab160e6bbb0e7a79b3f8c3bc7
David Kernan <dave@paddez.com>
2026-03-04 12:58:47 +0000

    Fix build error: Missing white color resource in obsolete XML layouts. Bump version to 1.2.1.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.paddez.wordformed"
         minSdk = 24
         targetSdk = 35
-        versionCode = 7
-        versionName = "1.2.0"
+        versionCode = 8
+        versionName = "1.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
